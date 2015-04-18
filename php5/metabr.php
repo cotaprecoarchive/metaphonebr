c965970d785b3c0074623de2396d67eb3546c2e4
Andrey K <andreykvital@gmail.com>
2015-04-18 11:59:31 -0300

    Removing usless cast to (string)

--- php5/metabr.php
+++ php5/metabr.php
@@ -1,10 +1,10 @@
 <?php
 
-echo (string) metaphone_ptbr('feijão'), PHP_EOL; // FJ
-echo (string) metaphone_ptbr('feijoao'), PHP_EOL; // FJ
-echo (string) metaphone_ptbr('scheila'), PHP_EOL; // XL
-echo (string) metaphone_ptbr('cheila'), PHP_EOL; // XL
-echo (string) metaphone_ptbr('andrey'), PHP_EOL; // ANDR
-echo (string) metaphone_ptbr('audrei'), PHP_EOL; // ADR
-echo (string) metaphone_ptbr('andruei'), PHP_EOL; // ANDR
-echo (string) metaphone_ptbr('andrei'), PHP_EOL; // ANDR
+echo metaphone_ptbr('feijão'), PHP_EOL; // FJ
+echo metaphone_ptbr('feijoao'), PHP_EOL; // FJ
+echo metaphone_ptbr('scheila'), PHP_EOL; // XL
+echo metaphone_ptbr('cheila'), PHP_EOL; // XL
+echo metaphone_ptbr('andrey'), PHP_EOL; // ANDR
+echo metaphone_ptbr('audrei'), PHP_EOL; // ADR
+echo metaphone_ptbr('andruei'), PHP_EOL; // ANDR
+echo metaphone_ptbr('andrei'), PHP_EOL; // ANDR
