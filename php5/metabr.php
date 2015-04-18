e920665102ae77d58e459cba22fb0da992d979b0
Andrey K <andreykvital@gmail.com>
2015-04-18 11:46:02 -0300

    Adding metabr.php (...testing!)

---
+++ php5/metabr.php
@@ -0,0 +1,10 @@
+<?php
+
+echo (string) metaphone_ptbr('feijão'), PHP_EOL; // FJ
+echo (string) metaphone_ptbr('feijoao'), PHP_EOL; // FJ
+echo (string) metaphone_ptbr('scheila'), PHP_EOL; // XL
+echo (string) metaphone_ptbr('cheila'), PHP_EOL; // XL
+echo (string) metaphone_ptbr('andrey'), PHP_EOL; // ANDR
+echo (string) metaphone_ptbr('audrei'), PHP_EOL; // ADR
+echo (string) metaphone_ptbr('andruei'), PHP_EOL; // ANDR
+echo (string) metaphone_ptbr('andrei'), PHP_EOL; // ANDR
